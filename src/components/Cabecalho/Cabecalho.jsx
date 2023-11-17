aa19f739262820ee793c2cfae5700b808c3edfcc
Miguel-Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-17 20:48:31 -0300

    corpo do cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,9 +1,40 @@
-
+'use client'
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Cabecalho() {
+
+
+  const pathname = usePathname();
+
+
   return (
-    <div>
-      
-    </div>
+    <header>
+    <nav>
+      <div className='rotas'>
+        
+          <Link href="/" className={pathname === '/' ? 'active' : ''}>
+            Home
+          </Link>
+        
+        
+          <Link href="/consulta" className={pathname === '/ajuda' ? 'active' : ''}>
+            Consulta
+          </Link>
+        
+        
+          <Link href="/formulario" className={pathname === '/modals' ? 'active' : ''}>
+            Formulario
+          </Link>
+        
+        
+          <Link href="/login" className={pathname === '/sobre' ? 'active' : ''}>
+            Login
+          </Link>
+          
+      </div>
+    </nav>
+</header>
   )
 }
