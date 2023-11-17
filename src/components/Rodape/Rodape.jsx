888264ae21d2ddba3a8e9b63ce58fb61970e362e
Miguel-Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-17 20:45:04 -0300

    Corpo do rodape

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -1,8 +1,14 @@
 
 export default function Rodape() {
   return (
-    <div>
-      
-    </div>
+    <footer>
+        <div class="rodape">
+            <p> Alberto Seiji Assato - RM 99977 |
+                Matheus Rodrigues Oliveira - RM 551997 |
+                Miguel Francisco de Freitas Fernandes - RM 99997 |
+                Patrick de Lima Jaguski - RM 551521|
+                Ye - RM 552213 </p>
+        </div>
+    </footer>
   )
 }
