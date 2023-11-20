7b18d3e3c5778938c253273cbc44af6c3b0ddb71
patrickjaguski <patrick_jaguski@hotmail.com>
2023-11-20 14:43:27 -0300

    montando corpo do login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,28 @@
+"use client";
+import Link from 'next/link'
+
+
+
 
 export default function Login() {
   return (
-    <div>
-      
-    </div>
+  <main className="login">
+        <div className="form">
+          <form className="formulario">
+            <h1>Acessar Conta</h1>
+            <div>
+              <label htmlFor="email">Preencha seus dados de acesso para continuar.</label>
+              <input type="text" id="IdEmail" name='email' placeholder="Email"/>
+            </div>
+            <div>
+              <label htmlFor="Senha"></label>
+              <input type="password" id="IdSenha" name='senha' placeholder="Senha" />
+            </div>
+            <div className="register">
+              <input type="submit" id="Idlogar" value="Entrar" />
+            </div>
+          </form>
+        </div>
+      </main>
   )
 }
