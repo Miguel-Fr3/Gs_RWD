e7a80ba332d95689410089e535ffefc0cbee9f7b
luigi138 <nimapi20041012@gmail.com>
2023-11-21 13:59:19 -0300

    Alterando Rodape

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -3,12 +3,21 @@ export default function Rodape() {
   return (
     <footer>
         <div className="rodape">
-            <p> | Alberto Seiji Assato - RM 99977 | </p>
-            <p> | Matheus Rodrigues Oliveira - RM 551997 | </p>
-            <p> | Miguel Francisco de Freitas Fernandes - RM 99997 | </p>
-            <p> | Patrick de Lima Jaguski - RM 551521| </p>
-            <p> | Ye - RM 552213 | </p>
-        </div>
+
+            <div className='front-end'>
+              <h1>Front-End</h1>
+              <p> | Miguel Francisco de Freitas Fernandes - RM 99997 | </p>
+              <p> | Ye - RM 552213 | </p>
+            </div>
+
+            <div className='back-end'>
+            <h1>Back-End</h1>
+              <p> | Alberto Seiji Assato - RM 99977 | </p>
+              <p> | Matheus Rodrigues Oliveira - RM 551997 | </p>
+              <p> | Patrick de Lima Jaguski - RM 551521| </p>
+              </div>
+
+            </div>
     </footer>
   )
 }
