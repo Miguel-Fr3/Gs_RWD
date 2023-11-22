930e0eff1af47621bb97ff147c1d11c0dda59d34
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-22 18:30:53 -0300

    Iniciando função login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,86 @@
 "use client";
 import Link from 'next/link'
 import './login.scss'
-
-
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
 
 export default function Login() {
+
+    const navigate = useRouter();
+
+    const [Stats, setStats] = useState("");
+    const [classLoginMsg, setClassLoginMsg] = useState("");
+
+    const [usuario, setUsuario] = useState({
+        "info":"login",
+        "email":"",
+        "senha":""
+    });
+
+    useEffect(() => {
+       if(Stats == "SUCESSO!"){
+          setClassLoginMsg("suc");
+        }else if(Stats == "USUÁRIO E OU SENHA INVÁLIDOS!"){
+            setClassLoginMsg("err");
+        }else{
+            setClassLoginMsg("login");
+        }
+    }, [Stats]);
+    
+    //Função de preenchimento do FORM...
+    const handleChange = (e)=>{
+        //Destructuring
+        const{name, value} = e.target;
+        //Prenchendo o campo, utilizando o useState com SPREAD + OnChange:
+        setUsuario({...usuario,[name]:value});
+    }
+
+    //Função de validação e ENVIO dos dados.
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:3000/api/base/base-users/0",{
+                method: "POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:  JSON.stringify(usuario)
+            });
+
+            if(response.ok){
+                const user = await response.json();
+
+                if(user){
+
+                    //Gerando o token do usuário:
+                    const token = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+
+                    //Armazenar o token no sessionStorage:
+                    sessionStorage.setItem("token-user",token);
+
+                    setMsgStatus("SUCESSO!");
+                    
+                    setTimeout(()=>{
+                        setMsgStatus("");
+                        navigate.push("/");
+                    },5000);
+                }else{
+                    setMsgStatus("USUÁRIO E OU SENHA INVÁLIDOS!");
+                    setTimeout(()=>{
+                        setMsgStatus("");
+                        setUsuario({
+                            "info":"login",
+                            "email":"",
+                            "senha":""
+                        });
+                    },5000);
+                }
+            }
+        } catch (error) {
+      }
+    }
+
   return (
   <main className="login">
         <div className="form">
