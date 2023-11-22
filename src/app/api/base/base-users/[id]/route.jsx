b9049960eae7567bdc42a39da0f9570cca331898
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-22 19:01:40 -0300

    Criando cadastro

--- src/app/api/base/base-users/[id]/route.jsx
+++ src/app/api/base/base-users/[id]/route.jsx
@@ -16,7 +16,7 @@ export async function GET(request, {params}) {
     }
 }
 
-//Criando a função do LOGIN
+
 const handleLogin = async (email,senha)=>{
     const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
     const usuarios = await JSON.parse(file);
@@ -35,28 +35,26 @@ const handleLogin = async (email,senha)=>{
  }
 }
 
-//Criando a função do CADASTRO
-const handleCadastrar = async (email,nome,senha)=>{
+
+const handleCadastrar = async (email,senha)=>{
     const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
     const usuarios = await JSON.parse(file);
 
     try{
         
-        //Gerando o ID do usuário:
+
         const id =  (usuarios.usuarios[usuarios.usuarios.length-1].id + 1);
 
-        //Criando o objeto do usuário:
         const user = {
             "id":id,
-            "nome":nome,
             "email":email,
             "senha":senha
         }
 
-        //Adicionando o usuário no array:
+
         usuarios.usuarios.push(user);
 
-        //Salvando o arquivo:
+
         await fs.writeFile(process.cwd() + '/src/app/api/base/data.json', JSON.stringify(usuarios));
 
         return user;
@@ -66,15 +64,15 @@ const handleCadastrar = async (email,nome,senha)=>{
 }
 
 export async function POST(request, response){
-    //Pegando os dados do request com o await e destructuring.
-    const {info,email,nome,senha} = await request.json();
 
-    console.log(info,nome,email,senha);
+    const {info,email,senha} = await request.json();
+
+    console.log(info,email,senha);
 
     if(info == "login"){
         return  NextResponse.json( await handleLogin(email,senha));
     } else if(info == "cadastro"){
-        return  NextResponse.json( await handleCadastrar(email,nome,senha));
+        return  NextResponse.json( await handleCadastrar(email,senha));
     }
     return NextResponse.json({"status":false});
 }
--- src/app/login/[cad]/page.jsx
+++ src/app/login/[cad]/page.jsx
@@ -1,21 +1,94 @@
 "use client";
 import Link from 'next/link'
 import '../login.scss'
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 
 export default function Cadastro() {
+
+  
+    const router = useRouter();
+
+    const [Stats, setStats] = useState("");
+    const [classMsg, setClassMsg] = useState("");
+
+
+    const [usuario, setUsuario] = useState({
+        "info":"cadastro",
+        "email":"",
+        "senha":""
+    });
+
+    useEffect(() => {
+       if(Stats == "SUCESSO!"){
+          setClassMsg("suc");
+        }else if(Stats == "OCORREU UM ERRO!"){
+            setClassMsg("err");
+        }else{
+            setClassMsg("login");
+        }
+    }, [Stats]);
+    
+
+    const handleChange = (e)=>{
+
+        const{name, value} = e.target;
+
+        setUsuario({...usuario,[name]:value});
+    }
+
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        
+        try {
+            const response = await fetch("http://localhost:3000/api/base/base-users/0",{
+                method: "POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:  JSON.stringify(usuario)
+            });
+
+            if(response.ok){
+                const user = await response.json();
+
+                if(user){
+                    setStats("SUCESSO!");
+                    setTimeout(()=>{
+                        setStats("");
+                        router.push("/");
+                    },5000);
+                }else{
+                    setStats("OCORREU UM ERRO!");
+                    setTimeout(()=>{
+                        setStats("");
+                        setUsuario({
+                            "info":"cadastro",
+                            "email":"",
+                            "senha":""
+                        });
+                    },5000);
+                }
+            }
+        } catch (error) {
+      }
+    }
+
   return (
     <main className="login">
     <div className="form">
-      <form className="formulario">
+      <form className="formulario" onSubmit={handleSubmit}>
         <h1>Cadastro</h1>
+        <h2 className={classMsg}>{Stats}</h2>
         <div>
           <label htmlFor="IdEmail">Preencha seus dados</label>
-          <input type="text" id="IdEmail" name='email' placeholder="Email" autoComplete="email"/>
+          <input type="text" id="IdEmail" name='email' placeholder="Email" autoComplete="email" value={usuario.email} onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor="IdSenha">Crie sua senha</label>
-          <input type="password" id="IdSenha" name='senha' placeholder="Senha" autoComplete="current-password"/>
+          <input type="password" id="IdSenha" name='senha' placeholder="Senha" autoComplete="current-password" value={usuario.senha} onChange={handleChange}/>
         </div>
         <div className="register">
           <input type="submit" id="IdCad" value="Entrar" />
