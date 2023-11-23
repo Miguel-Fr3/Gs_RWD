d3dcfa3464101fa131a9705e223c917b7d04a49d
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-23 16:58:35 -0300

    Alterando alts E adicionando imgs

--- src/app/consulta/page.jsx
+++ src/app/consulta/page.jsx
@@ -1,8 +1,12 @@
 import './consulta.scss'
+import Image from 'next/image'
 export default function Consulta() {
   return (
     <main className="consulta">
       <h1>Consulta</h1>
+      <div>
+      <Image src="/img5.png" alt="Imagem de gestão de tempo" width={200} height={140} priority={true} className='img'/>
+      </div>
     </main>
   )
 }
