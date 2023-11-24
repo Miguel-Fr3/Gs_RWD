c399a9edf21ff00fa7aea8d47e67553d1a16c35a
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-24 04:29:39 -0300

    Tentando fazer o login com java

--- src/app/api/base/base-users/[id]/route.jsx
+++ src/app/api/base/base-users/[id]/route.jsx
@@ -1,78 +1,89 @@
-import {promises as fs} from 'fs';
+
+import { promises as fs } from 'fs';
 import { NextResponse } from 'next/server';
 
-export async function GET(request, {params}) {
-    
-    const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
+const filePath = process.cwd() + 'http://localhost:8080/api/login';
 
-    const id = params.id;
+export async function GET(request, { params }) {
+    try {
 
-    const usuarios =  await JSON.parse(file);
-    
-    if(id > 0 && id <= usuarios.usuarios.length){
-        return  NextResponse.json(usuarios.usuarios.find((user)=> user.id == id));
-    }else{
-        return id == 0 ? NextResponse.json(usuarios.usuarios) : NextResponse.redirect("http://localhost:3000/error")
+        const fileContent = await fs.readFile(filePath, 'utf8');
+        
+
+        const usuarios = JSON.parse(fileContent);
+
+        const id = params.id;
+        
+        if (id > 0 && id <= usuarios.usuarios.length) {
+            return NextResponse.json(usuarios.usuarios.find((user) => user.id == id));
+        } else {
+            return id == 0 ? NextResponse.json(usuarios.usuarios) : NextResponse.redirect("http://localhost:3000/error");
+        }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.error(error);
     }
 }
 
+const handleLogin = async (email, senha) => {
+    try {
+        const fileContent = await  fs.readFile(process.cwd() + 'http://localhost:8080/api/login/autenticar', 'utf8');
+        const usuarios = JSON.parse(fileContent);
 
-const handleLogin = async (email,senha)=>{
-    const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
-    const usuarios = await JSON.parse(file);
-
-    try{
         for (let x = 0; x < usuarios.usuarios.length; x++) {
             const userFile = usuarios.usuarios[x];
 
-            if(userFile.email == email && userFile.senha == senha){
+            if (userFile.email == email && userFile.senha == senha) {
                 return userFile;
             }
         }
         return null;
-    }catch(error){
-        console.log(error);
- }
-}
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const handleCadastrar = async (email, senha) => {
+    const fileContent =  await  fs.readFile(process.cwd() + 'http://localhost:8080/api/login', 'utf8');
+    const usuarios = JSON.parse(fileContent);
+    try {
 
 
-const handleCadastrar = async (email,senha)=>{
-    const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
-    const usuarios = await JSON.parse(file);
-
-    try{
-        
-
-        const id =  (usuarios.usuarios[usuarios.usuarios.length-1].id + 1);
+        const id = (usuarios.usuarios[usuarios.usuarios.length - 1].id + 1);
 
         const user = {
-            "id":id,
-            "email":email,
-            "senha":senha
-        }
-
+            "id": id,
+            "email": email,
+            "senha": senha
+        };
 
         usuarios.usuarios.push(user);
 
 
-        await fs.writeFile(process.cwd() + '/src/app/api/base/data.json', JSON.stringify(usuarios));
+        await fs.writeFile(process.cwd() + 'http://localhost:8080/api/login', JSON.stringify(usuarios));
 
         return user;
-    }catch(error){
-        console.log(error);
- }
-}
+    } catch (error) {
+        console.error(error);
 
-export async function POST(request, response){
-
-    const {info,email,senha} = await request.json();
-
-    console.log(info,email,senha);
-
-    if(info == "login"){
-        return  NextResponse.json( await handleLogin(email,senha));
-    } else if(info == "cadastro"){
-        return  NextResponse.json( await handleCadastrar(email,senha));
     }
-    return NextResponse.json({"status":false});
+};
+
+export async function POST(request, response) {
+    try {
+        const { info, email, senha } = await request.json();
+
+        console.log(info, email, senha);
+
+        if (info == "login") {
+            return NextResponse.json(await handleLogin(email, senha));
+        } else if (info == "cadastro") {
+            return NextResponse.json(await handleCadastrar(email, senha));
+        }
+
+        return NextResponse.json({ "status": false });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.error(error);
+    }
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,104 +1,148 @@
 "use client";
-import Link from 'next/link'
-import './login.scss'
+import Link from 'next/link';
+import './login.scss';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function Login() {
+  const navigate = useRouter();
 
-    const navigate = useRouter();
+  const [stats, setStats] = useState('');
+  const [classMsg, setClassMsg] = useState('');
 
-    const [Stats, setStats] = useState("");
-    const [classMsg, setClassMsg] = useState("");
+  const [usuario, setUsuario] = useState({
+    "info": "login",
+    "email": "",
+    "senha": "",
+  });
 
-    const [usuario, setUsuario] = useState({
-        "info":"login",
-        "email":"",
-        "senha":""
-    });
-
-    useEffect(() => {
-       if(Stats == "SUCESSO!"){
-          setClassMsg("suc");
-        }else if(Stats == "USUÁRIO E OU SENHA INVÁLIDOS!"){
-            setClassMsg("err");
-        }else{
-            setClassMsg("login");
-        }
-    }, [Stats]);
-    
-
-    const handleChange = (e)=>{
-
-        const{name, value} = e.target;
-
-        setUsuario({...usuario,[name]:value});
+  useEffect(() => {
+    if (stats === 'SUCESSO!') {
+      setClassMsg('suc');
+      setTimeout(() => {
+        setStats('');
+        navigate.push('/');
+      }, 5000);
+    } else if (stats === 'USUÁRIO E OU SENHA INVÁLIDOS!') {
+      setClassMsg('err');
+      setTimeout(() => {
+        setStats('');
+        setUsuario({
+          info: 'login',
+          email: '',
+          senha: '',
+        });
+      }, 5000);
+    } else {
+      setClassMsg('login');
     }
+  }, [stats, navigate]);
 
 
-    const handleSubmit = async (e)=>{
-        e.preventDefault();
 
-        try {
-            const response = await fetch("http://localhost:3000/api/base/base-users/0",{
-                method: "POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body:  JSON.stringify(usuario)
-            });
+  
 
-            if(response.ok){
-                const user = await response.json();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUsuario({ ...usuario, [name]: value });
+  };
 
-                if(user){
 
-                    const token = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
-
-                    sessionStorage.setItem("token-user",token);
-
-                    setStats("SUCESSO!");
-                    
-                    setTimeout(()=>{
-                        setStats("");
-                        navigate.push("/");
-                    },5000);
-                }else{
-                    setStats("USUÁRIO E OU SENHA INVÁLIDOS!");
-                    setTimeout(()=>{
-                        setStats("");
-                        setUsuario({
-                            "info":"login",
-                            "email":"",
-                            "senha":""
-                        });
-                    },5000);
-                }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch('http://localhost:8080/api/login/autenticar', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+      });
+  
+      console.log('Response:', response);
+  
+      if (response.ok) {
+        const responseBody = await response.json();
+  
+        console.log('Response Text:', responseBody);
+  
+        if (responseBody) {
+          try {
+            const user = JSON.parse(responseBody);
+            console.log('Parsed JSON:', user);
+  
+            if (user) {
+              const token =
+                Math.random().toString(36).substring(2) +
+                Math.random().toString(36).substring(2);
+  
+              sessionStorage.setItem('token-user', token);
+              setStats('SUCESSO!');
+            } else {
+              setStats('USUÁRIO E OU SENHA INVÁLIDOS!');
             }
-        } catch (error) {
+          } catch (jsonError) {
+            console.error('Erro ao analisar JSON da resposta:', jsonError);
+            console.error('Resposta do servidor:', response);
+            setStats('Ocorreu um erro na autenticação.');
+          }
+        } else {
+          console.error('Corpo da resposta está vazio.');
+          setStats('Ocorreu um erro na autenticação.');
+        }
+      } else {
+        console.error('Erro na requisição:', response.status);
+        console.error('Resposta do servidor:', response);
+        setStats('Ocorreu um erro na autenticação.');
       }
+    } catch (error) {
+      console.error('Erro na autenticação:', error);
+      setStats('Ocorreu um erro na autenticação.');
     }
+  };
+  
+
+  
 
   return (
-  <main className="login">
-        <div className="form">
-          <form className="formulario" onSubmit={handleSubmit}>
-            <h1>Login</h1>
-            <h2 className={classMsg}>{Stats}</h2>
-            <div>
-              <label htmlFor="IdEmail">Preencha seus dados</label>
-              <input type="text" id="IdEmail" name='email' placeholder="Email" autoComplete="email" value={usuario.email} onChange={handleChange}/>
-            </div>
-            <div>
-              <label htmlFor="IdSenha">Digite sua senha</label>
-              <input type="password" id="IdSenha" name='senha' placeholder="Senha" autoComplete="current-password" value={usuario.senha} onChange={handleChange}/>
-            </div>
-            <div className="register">
-              <input type="submit" id="Idlogar" value="Entrar" />
-            </div>
-            <Link href="/login/cadastro" className='caminho'>Não tem cadastro? Clique aqui!</Link>
-          </form>
-        </div>
-      </main>
-  )
+    <main className="login">
+      <div className="form">
+        <form className="formulario" onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <h2 className={classMsg}>{stats}</h2>
+          <div>
+            <label htmlFor="IdEmail">Preencha seus dados</label>
+            <input
+              type="text"
+              id="IdEmail"
+              name="email"
+              placeholder="Email"
+              autoComplete="email"
+              value={usuario.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="IdSenha">Digite sua senha</label>
+            <input
+              type="password"
+              id="IdSenha"
+              name="senha"
+              placeholder="Senha"
+              autoComplete="current-password"
+              value={usuario.senha}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="register">
+            <input type="submit" id="Idlogar" value="Entrar" />
+          </div>
+          <Link href="/login/cadastro" className="caminho">
+            Não tem cadastro? Clique aqui!
+          </Link>
+        </form>
+      </div>
+    </main>
+  );
 }
